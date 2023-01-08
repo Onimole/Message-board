d5c685fd1d444f9c71a860bcd8a33442147a4bfb
onimole <onimoletaofeek@gmail.com>
2023-01-08 01:32:37 +0100

    Do it

--- posts/models.py
+++ posts/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Post(models.Model):
-    text = models.TextField()
+    textyou = models.TextField()
 
     def __str__(self):
-        return self.text[:50]
+        return self.textyou[:50]
