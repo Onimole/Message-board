14b1ab3494b2e0c2f823427d451e1b4993f470dd
onimole <onimoletaofeek@gmail.com>
2023-01-08 02:30:51 +0100

    New updates for heroku deployment

--- Message_Box/settings.py
+++ Message_Box/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-g@2mdjqhi6(62-fgx$t=s62sez9%jvi!qdztbznue63^%lw-@&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".herokuapp.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
